6cbede36693600919d75ecb8018396606c09654e
Justin Dury-Agri <justin.dury.agri@gmail.com>
2020-08-20 01:36:37 -0400

    pigLatins webpages

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 // piglatin function
 // access the string, split correctly, for each word create a catch string, translate word to piglatin
-
 function pigTrans(string) {
-  const strArr = string.split(' ');
-  return strArr.map(word => {
-    return word
-    .replace(/^[aeiou]\w*/, "$&way")
-    .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
-  }).join(' ');
-}
-
-// console.log(pigTrans('thing, one and two'));
-document.querySelector('body').style.backgroundColor = 'green';
-
-
-
-
-// chrome.runtime.onInstalled.addListener(function(
-
-// need an event listener to trigger this script
-document.addEventListener('DOMContentLoaded', function(event) {
-// chrome.runtime.onInstalled.addListener(function() {
-  document.querySelector('body').style.backgroundColor = 'green';
-  // navigate the DOM
-    // if the element has a text value
-        // change the inner text to the value of invoking pigtrans with the text value as the arg
-});
-
-// $('element').value   
-//
-
+    if (typeof string !== 'string') return undefined;
+    const strArr = string.split(' ');
+    return strArr.map(word => {
+      return word
+      .replace(/^[aeiou]\w*/, "$&way")
+      .replace(/(^[^aeiou]+)(\w*)/, "$2$1ay");
+    }).join(' ');
+  }
+  document.querySelectorAll('h1, h2, p1, p2, h, p, p3, h3, title, dl').forEach(child => {
+    return child.innerHTML = pigTrans(child.innerHTML);
+  })
+  // console.log(pigTrans('thing, one and two'));
+  // document.querySelector('body').style.backgroundColor = 'fuchsia';
+  // recursively walk through the DOM
+  // function nodeWalker(node) {
+  //   //base case
+  //   if (node.firstChild === null) node.innerHTML = pigTrans(node.innerHTML);
+  //   // recursive case 
+  //   node.childNodes.forEach(child => {
+  //     // if (node.innerHTML) {
+  //     //   node.innerHTML = pigTrans(node.innerHTML);
+  //     // }
+  //     nodeWalker(child);
+  //   })
+  // }
+  // nodeWalker(document);
+  // /(<h\d?>)|(<p\d?>)/g
+  
